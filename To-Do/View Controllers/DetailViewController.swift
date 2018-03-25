e30b2923b092f062b9e809305956e9b690dc1b43
adria <Adrian_Avram@Outlook.com>
2018-03-24 20:36:10 -0700

    Worked on transferring index path to other swift file

--- To-Do/View Controllers/DetailViewController.swift
+++ To-Do/View Controllers/DetailViewController.swift
@@ -8,24 +8,27 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class DetailViewController: UIViewController //, UITableViewDelegate, UITableViewDataSource
+{
     
     
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var goalLabel: UILabel!
     @IBOutlet weak var topViewHeight: NSLayoutConstraint!
     @IBOutlet weak var goalTableView: UITableView!
-    
-    var listGoals = ["hello", "alan", "is", "good"]
+    var dataIndex : Int = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print(dataIndex)
         // Do any additional setup after loading the view.
-         topViewHeight.constant = (self.view.frame.size.height / 4.5) - 10
+//         topViewHeight.constant = (self.view.frame.size.height / 4.5) - 10
         
         
         // Set yourself as a delegate and datasource here:
-        goalTableView.delegate = self;
-        goalTableView.dataSource = self;
+//        goalTableView.delegate = self;
+//        goalTableView.dataSource = self;
         
     }
 
@@ -34,23 +37,20 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
         // Dispose of any resources that can be recreated.
     }
    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listGoals.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
-        cell.goalLabel.text = listGoals[indexPath.row]
-        
-        return cell; 
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//       let selectedIndex = indexPath.row
-        performSegue(withIdentifier: "goToSpecific", sender: self)
-    }
-
-  
-
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return listGoals.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
+//        cell.goalLabel.text = listGoals[indexPath.row]
+//
+//        return cell;
+//    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+////       let selectedIndex = indexPath.row
+//        performSegue(withIdentifier: "goToSpecific", sender: self)
+//    }
 }
--- To-Do/View Controllers/ViewController.swift
+++ To-Do/View Controllers/ViewController.swift
@@ -14,24 +14,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var section: UITableView!
     var sections = ["hi", "awee", "is", "gya"]
+    var tableIndex = -1
     @IBOutlet weak var topViewHeight: NSLayoutConstraint!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       section.delegate = self
+        section.delegate = self
         section.dataSource = self
 
         self.navigationController?.navigationBar.isHidden = true
         topViewHeight.constant = (self.view.frame.size.height / 4.5) - 10
         // Do any additional setup after loading the view.
-        
-      
-        
-        
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationView : DetailViewController = segue.destination as! DetailViewController
+        destinationView.dataIndex = tableIndex
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -39,6 +40,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        tableIndex = indexPath.row
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
